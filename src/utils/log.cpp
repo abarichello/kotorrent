3f5ad2d2ee26e1e42751c1bf232c2e1133ea13c9
Barichello <artur@barichello.me>
2021-07-01 18:36:21 -0300

    Add datetime to log

--- src/utils/log.cpp
+++ src/utils/log.cpp
@@ -8,27 +8,33 @@ void Log::messageHandler(QtMsgType type, const QMessageLogContext& context, cons
     outFile.open(QIODevice::WriteOnly | QIODevice::Append);
     QTextStream ts(&outFile);
 
-    QString lineFormat = "%1%2 (%3:%4, %5)" + Qt::endl;
+    QString lineFormat = "%1|%2|%3 (%4:%5, %6)\n";
+    auto datetime = QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss");
     QString lineOutput;
     auto outputFunction = qDebug();
     switch (type) {
         case QtDebugMsg:
-            lineOutput = lineFormat.arg("DEBUG:", msg, context.file, QString::number(context.line), context.function);
+            lineOutput = lineFormat.arg("[DEBUG]", datetime, msg, context.file, QString::number(context.line),
+                                        context.function);
             break;
         case QtInfoMsg:
-            lineOutput = lineFormat.arg("INFO: ", msg, context.file, QString::number(context.line), context.function);
+            lineOutput = lineFormat.arg("[INFO ]", datetime, msg, context.file, QString::number(context.line),
+                                        context.function);
             outputFunction = qInfo();
             break;
         case QtWarningMsg:
-            lineOutput = lineFormat.arg("WARN :", msg, context.file, QString::number(context.line), context.function);
+            lineOutput = lineFormat.arg("[WARN ]", datetime, msg, context.file, QString::number(context.line),
+                                        context.function);
             outputFunction = qWarning();
             break;
         case QtCriticalMsg:
-            lineOutput = lineFormat.arg("CRTCL:", msg, context.file, QString::number(context.line), context.function);
+            lineOutput = lineFormat.arg("[CRTCL]", datetime, msg, context.file, QString::number(context.line),
+                                        context.function);
             outputFunction = qCritical();
             break;
         case QtFatalMsg:
-            lineOutput = lineFormat.arg("FATAL:", msg, context.file, QString::number(context.line), context.function);
+            lineOutput = lineFormat.arg("[FATAL]", datetime, msg, context.file, QString::number(context.line),
+                                        context.function);
             outputFunction = qCritical();
             abort();
     }
